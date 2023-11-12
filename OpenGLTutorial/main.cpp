c6a945eabc27099a6e8fb8c134865a15cbd4e16a
Murat Eksi <murat@eksiventures.com>
2023-11-12 18:48:19 +0100

    add shaders

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -7,7 +7,98 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+    
+    enum class ShaderType
+    {
+        NONE = -1, VERTEX = 0, FRAGMENT = 1
+    };
+    
+    std::string line;
+    std::stringstream ss[2];
+    
+    ShaderType type = ShaderType::NONE;
+    
+    if (!stream.is_open())
+        std::cout << "Failed to open " << filepath << "\n" << std::endl;
+    
+    while(getline(stream, line))
+    {
+        if (line.find("#shader") != std::string::npos)
+        {
+            if(line.find("vertex") != std::string::npos)
+            {
+                type = ShaderType::VERTEX;
+            }
+            else if (line.find("fragment") != std::string::npos)
+            {
+                type = ShaderType::FRAGMENT;
+            }
+        }
+        else
+        {
+            ss[(int)type] << line << "\n";
+        }
+        
+    }
+    
+    return {
+        ss[0].str(), ss[1].str()
+    };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        
+        std::cout << "Failder to compile " << (type ==GL_VERTEX_SHADER ? "vertex" : "fragment" ) << " shader!" << std::endl;
+        std::cout << message << std::endl;
+        return 0;
+    }
+    
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+    
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    
+    return program;
+}
 
 
 int main(void)
@@ -17,6 +108,14 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__
+        std::cout << "I'm an Apple machine" << std::endl;
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
     
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -34,9 +133,13 @@ int main(void)
     
     float positions[6] = {
         -0.5f, -0.5f,
-        0.5f, 0.5f,
+        0.0f, 0.5f,
         0.5f, -0.5f
     };
+    
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
     
     unsigned int buffer;
     glGenBuffers(1, &buffer);
@@ -45,6 +148,11 @@ int main(void)
     
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
+    
+    ShaderProgramSource source = ParseShader("Basic.glsl");
+
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    glUseProgram(shader);
     
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
