5e2a75310086361029d4b46e8a7afbd832e3a0ee
Murat Eksi <murat@eksiventures.com>
2023-11-12 13:23:23 +0100

    Initial Commit

---
+++ OpenGLTutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLTutorial
+//
+//  Created by Murat Eksi on 2023-11-12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
