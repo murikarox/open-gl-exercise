cc87346395fd7e10d627cdb54d412c916c2e11f5
Murat Eksi <murat@eksiventures.com>
2023-11-13 19:05:16 +0100

    add uniform example

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -148,6 +148,8 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    
+    GLCall(glfwSwapInterval(1));
             
     if (glewInit() != GLEW_OK)
         std::cout << "Error!";
@@ -186,12 +188,26 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
     
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+    
+    float r = 0.0f;
+    float increment = 0.05f;
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+        
+        r += increment;
         
         /* Swap front and back buffers */
         GLCall(glfwSwapBuffers(window));
